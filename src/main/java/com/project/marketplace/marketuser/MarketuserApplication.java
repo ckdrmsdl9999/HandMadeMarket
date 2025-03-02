7928444ab96cb376b90dd01ad116893c24a6f5d0
CGYOON <ckdrmsdl9999@naver.com>
2025-03-02 22:37:48 +0900

    init commit

---
+++ src/main/java/com/project/marketplace/marketuser/MarketuserApplication.java
@@ -0,0 +1,13 @@
+package com.project.marketplace.marketuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/marketplace/marketuser/MarketuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.marketplace.marketuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
