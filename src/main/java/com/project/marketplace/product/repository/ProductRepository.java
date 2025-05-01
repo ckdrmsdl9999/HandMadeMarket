10fa9b48bebf5bab76d997883ca6b8f5ced8cbcb
changgeun <ckdrmsdl9999@naver.com>
2025-05-01 23:52:49 +0900

    fix : 인기상품 조회 쿼리 수정

--- src/main/java/com/project/marketplace/product/repository/ProductRepository.java
+++ src/main/java/com/project/marketplace/product/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.project.marketplace.product.repository;
 
 
 import com.project.marketplace.product.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,7 +16,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // 상품명으로 검색 (이름에 키워드 포함된 상품)
     List<Product> findByNameContaining(String keyword);
 
-    // 인기 상품 조회 (판매 수량 기준 상위 n개)
-    @Query("SELECT p FROM Product p ORDER BY p.salesCount DESC LIMIT ?1")
-    List<Product> findPopularProducts(int limit);
+
+    @Query("SELECT p FROM Product p ORDER BY p.salesCount DESC")
+    List<Product> findPopularProducts(Pageable pageable);
 }
