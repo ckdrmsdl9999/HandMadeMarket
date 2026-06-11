bd81ae3308632fdd16eb7406a947ad05453c107b
changgeun <ckdrmsdl9999@naver.com>
2026-06-12 03:02:23 +0900

    ci: 테스트용 설정 추가

--- src/test/java/com/project/marketplace/MarketuserApplicationTests.java
+++ src/test/java/com/project/marketplace/MarketuserApplicationTests.java
@@ -2,7 +2,10 @@ package com.project.marketplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+// CI에서 운영 설정 대신 테스트 전용 H2 설정으로 컨텍스트를 검증하도록 추가함
+@ActiveProfiles("test")
 @SpringBootTest
 class MarketuserApplicationTests {
 
